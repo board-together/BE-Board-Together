eccb79d4c405e61b2e4c1b41dfe19cb45687c0c0
Darby Smith <newman.darby2@gmail.com>
2023-02-05 11:52:53 -0500

    Update testing and model to match new schema

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe User, type: :model do
   end
 
   describe 'validations' do
-    it { should validate_uniqueness_of :email }
-    it { should validate_presence_of :name}
+    it { should validate_uniqueness_of :username }
   end
 end
