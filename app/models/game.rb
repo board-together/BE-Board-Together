5cc80a2f4eb53c14eba60e15153a19c8d0fb2216
bkeener <bkeener7@gmail.com>
2023-02-06 07:41:40 -0700

    Formatter run

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,5 @@ class Game < ApplicationRecord
   has_many :user_games
   has_many :users, through: :user_games
 
-  validates_presence_of :board_game_atlas_id, :name, :min_players, :max_players, :min_playtime, :max_playtime, :min_age, :year_published, :description, :thumb_url, :image_url, :url
+  validates :board_game_atlas_id, :name, :min_players, :max_players, :min_playtime, :max_playtime, :min_age, :year_published, :description, :thumb_url, :image_url, :url, presence: true
 end
