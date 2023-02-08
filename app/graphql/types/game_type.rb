215bb32403260317313c44aeee33b7c3c70fe5bc
bkeener <bkeener7@gmail.com>
2023-02-08 10:47:54 -0700

    Format graphql files

--- app/graphql/types/game_type.rb
+++ app/graphql/types/game_type.rb
@@ -2,20 +2,20 @@
 
 module Types
   class GameType < Types::BaseObject
-    field :id, ID, null: false
     field :board_game_atlas_id, String
-    field :name, String
-    field :min_players, Integer
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :description, String
+    field :id, ID, null: false
+    field :image_url, String
     field :max_players, Integer
-    field :min_playtime, Integer
     field :max_playtime, Integer
     field :min_age, Integer
-    field :year_published, Integer
-    field :description, String
+    field :min_players, Integer
+    field :min_playtime, Integer
+    field :name, String
     field :thumb_url, String
-    field :image_url, String
-    field :url, String
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :url, String
+    field :year_published, Integer
   end
 end
--- app/graphql/types/user_game_type.rb
+++ app/graphql/types/user_game_type.rb
@@ -2,11 +2,11 @@
 
 module Types
   class UserGameType < Types::BaseObject
-    field :id, ID, null: false
-    field :user_id, Integer
-    field :game_id, Integer
-    field :status, Integer
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :game_id, Integer
+    field :id, ID, null: false
+    field :status, Integer
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :user_id, Integer
   end
 end
