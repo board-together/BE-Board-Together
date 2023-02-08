cd311e503bf4c977954684f9507bdf14bea1f7ff
bkeener <bkeener7@gmail.com>
2023-02-08 15:15:21 -0700

    Standardize some class structures

--- app/graphql/types/user_game_type.rb
+++ app/graphql/types/user_game_type.rb
@@ -1,12 +1,8 @@
-# frozen_string_literal: true
-
-module Types
-  class UserGameType < Types::BaseObject
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :game_id, Integer
-    field :id, ID, null: false
-    field :status, Integer
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :user_id, Integer
-  end
+class Types::UserGameType < Types::BaseObject
+  field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+  field :game_id, Integer
+  field :id, ID, null: false
+  field :status, Integer
+  field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  field :user_id, Integer
 end
