84a7096948bba8a05c04526f3875b2f2bd5684c9
bkeener <bkeener7@gmail.com>
2023-02-10 13:16:36 -0700

    Formatter run

--- app/graphql/types/user_game_type.rb
+++ app/graphql/types/user_game_type.rb
@@ -1,12 +1,12 @@
 class Types::UserGameType < Types::BaseObject
+  field :borrower_id, Integer, null: true
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+  field :game, Types::GameType, null: false
   field :game_id, Integer
   field :id, ID, null: false
   field :status, Integer
   field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   field :user_id, Integer
-  field :game, Types::GameType, null: false
-  field :borrower_id, Integer, null: true
 
   delegate :game, to: :object
 end
