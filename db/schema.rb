9a689c5116bfc638734934a83873a1515fd98cab
Annie Pulzone <ajpulzone@gmail.com>
2023-02-11 16:12:44 -0800

    ajp/update user_games table to include default of 0 for status attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_08_225455) do
+ActiveRecord::Schema.define(version: 2023_02_11_034810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,10 +35,10 @@ ActiveRecord::Schema.define(version: 2023_02_08_225455) do
   create_table "user_games", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "game_id"
-    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "borrower_id"
+    t.integer "status", default: 0
     t.index ["game_id"], name: "index_user_games_on_game_id"
     t.index ["user_id"], name: "index_user_games_on_user_id"
   end
