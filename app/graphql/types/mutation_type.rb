4a4add7c0dfd7a2756b9f1ab32922c3ddf02704e
Annie Pulzone <ajpulzone@gmail.com>
2023-02-11 16:16:36 -0800

    ajp/update to include update_user_game mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,4 +1,5 @@
 class Types::MutationType < Types::BaseObject
   field :create_user, mutation: Mutations::CreateUser
   field :create_user_game, mutation: Mutations::CreateUserGame
+  field :update_user_game, mutation: Mutations::UpdateUserGame
 end
