3b4d4f162966aeffb97244c310cf2589abb7350d
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-02-12 06:09:34 -0700

    Completes testing and implementation of delete_user mutation
    Why is this PR being made?
    
    To merge the testing and implementation of delete_user mutation
    
    How is this accomplished?
    
    writing of the delete_user_spec.rb
    
    Co-authored-by: Annie Pulzone <102780642+ajpulzone@users.noreply.github.com>
    Co-authored-by: Darby Smith <60988144+DarbySmith@users.noreply.github.com>
    Co-authored-by: Bryan Keener <101418582+bkeener7@users.noreply.github.com>

--- spec/graphql/mutations/delete_user_spec.rb
+++ spec/graphql/mutations/delete_user_spec.rb
@@ -1,28 +1,72 @@
 require 'rails_helper'
 
 describe Mutations::DeleteUser, type: :request do
-  let!(:user) { create(:user) }
+  before(:each) do
+    @user = create(:user)
+  end
 
   describe 'happy path' do
     it 'deletes a user via mutation' do
-      require 'pry'; binding.pry
-      post '/graphql', params: { query: mutation }
+      expect(User.count).to eq(1)
+      post '/graphql', params: { query: happy_path_mutation }
 
       expect(response).to be_successful
       parsed_response = JSON.parse(response.body, symbolize_names: true)
-      require 'pry'; binding.pry
+
+      expect(parsed_response).to be_a Hash
+      expect(parsed_response).to have_key :data
+      expect(parsed_response[:data]).to be_a Hash
+      expect(parsed_response[:data]).to have_key :deleteUser
+      expect(parsed_response[:data][:deleteUser]).to be_a Hash
+      expect(parsed_response[:data][:deleteUser]).to have_key :id
+      expect(parsed_response[:data][:deleteUser][:id]).to eq("#{@user.id}")
+      expect(User.count).to eq(0)
     end
   end
 
-  def mutation
+  describe 'sad path' do
+    it 'won\'t delete if a non-existent user is passed via mutation' do
+      expect(User.count).to eq(1)
+      post '/graphql', params: { query: sad_path_mutation }
+
+      expect(response).to be_successful
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed_response).to be_a Hash
+      expect(parsed_response).to have_key :data
+      expect(parsed_response[:data]).to be_a Hash
+      expect(parsed_response[:data]).to have_key :deleteUser
+      expect(parsed_response[:data][:deleteUser]).to be_a Hash
+      expect(parsed_response[:data][:deleteUser]).to have_key :id
+      expect(parsed_response[:data][:deleteUser][:id]).not_to eq("#{@user.id}")
+      expect(parsed_response[:data][:deleteUser]).to have_key :errors
+      expect(parsed_response[:data][:deleteUser][:errors]).to be_an Array
+      expect(parsed_response[:data][:deleteUser][:errors][0]).to be_a String
+      expect(parsed_response[:data][:deleteUser][:errors][0]).to eq('User does not exist')
+      expect(User.count).to eq(1)
+    end
+  end
+
+  def happy_path_mutation
     <<~GQL
       mutation {
-      deleteUser(input: {
-          id: #{user.id},
-          }) {
-          user {
-            id
-          }
+        deleteUser(input :{
+         id: "#{@user.id}",
+        }) {
+          id
+          errors
+        }
+      }
+    GQL
+  end
+
+  def sad_path_mutation
+    <<~GQL
+      mutation {
+        deleteUser(input :{
+         id: "999",
+        }) {
+          id
+          errors
         }
       }
     GQL
