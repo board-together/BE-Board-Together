b982663bca254ef39feed4c015080218a7315406
Annie Pulzone <ajpulzone@gmail.com>
2023-02-11 16:14:27 -0800

    ajp/update user_game factory borrower_id attribute to have nil as an option to account for unborrowed games

--- spec/factories/user_game.rb
+++ spec/factories/user_game.rb
@@ -4,11 +4,6 @@ FactoryBot.define do
     borrower_id { [nil, Faker::Number.within(range: 0..2)].sample }
     association :user, factory: :user
     association :game, factory: :game
-    
-    after(:build) do |user_game, evaluator|
-      if user_game.borrower_id.present?
-        user_game.status = 1
-      end
-    end
   end
 end 
+ 
