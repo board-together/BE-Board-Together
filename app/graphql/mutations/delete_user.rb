59524bfb9c7f15d20a45e4267fd565666ab1d23f
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-02-11 17:19:54 -0700

    Adds dependency to User model to destroy related objects
    Why is this PR being made?
    to enable the destruction of a user (and related objects)
    
    How is this accomplished?
    By adding `dependent: :destroy` to User model relationships
    
    Co-authored-by: Annie Pulzone <102780642+ajpulzone@users.noreply.github.com>
    Co-authored-by: Darby Smith <60988144+DarbySmith@users.noreply.github.com>
    Co-authored-by: Bryan Keener <101418582+bkeener7@users.noreply.github.com>

---
+++ app/graphql/mutations/delete_user.rb
@@ -0,0 +1,16 @@
+class Mutations::DeleteUser < Mutations::BaseMutation
+  # when a mutation is POSTed an :id argument is required
+  argument :id, ID, required: true
+
+  field :errors, [String], null: true
+  field :id, Types::UserType, null: true
+
+  def resolve(id:)
+    user = User.find_by(id: id)
+    
+    return { errors: [], id: nil } if user.nil?
+
+    user.destroy
+    { errors: [], id: user.id }
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 class Types::MutationType < Types::BaseObject
   field :create_user, mutation: Mutations::CreateUser
   field :create_user_game, mutation: Mutations::CreateUserGame
+  field :delete_user, mutation: Mutations::DeleteUser
   field :delete_user_game, mutation: Mutations::DeleteUserGame
 end
