10c128b51b7dbbcc494a800d979f0e8f5e0c2b0c
bkeener <bkeener7@gmail.com>
2023-07-17 13:53:43 -0600

    Refactor FactoryBot factories to prevent false failing tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :user, class: User do
-    username { Faker::Internet.username }
+    username { Faker::Internet.unique.username }
   end
 end
--- spec/factories/user_game.rb
+++ spec/factories/user_game.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user_game, class: UserGame do
     status { Faker::Number.within(range: 0..2) }
-    borrower_id { [nil, Faker::Number.within(range: 0..2)].sample }
+    borrower_id { [nil, User.pluck(:id).sample].sample }
     user factory: %i[user]
     game factory: %i[game]
   end
